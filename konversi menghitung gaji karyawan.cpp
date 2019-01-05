ab6051db1627e719cb14aa96c60840292c6df8f4
septianaana <45724156+septianaana@users.noreply.github.com>
2019-01-05 12:56:35 +0700

    Add files via upload

---
+++ konversi menghitung gaji karyawan.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int gaji;
+    int jam;
+    char nama;
+    cout<<"program input gaji karyawan\n"<<endl;
+    cout<<"gaji karyawan rp.5000 perjam dalam hari kerja 8jam"<<endl;
+    cout<<"masukan nama karyawan=";
+    cin>>nama;
+    cout<<"---------------------------------------"<<endl;
+        gaji=8*5000;
+    cout<<"total gaji anda";
+    cout<<"rp."<<gaji<<endl;
+    cout<<"----------------terima kasih --------------"<<endl;
+
+
+}
